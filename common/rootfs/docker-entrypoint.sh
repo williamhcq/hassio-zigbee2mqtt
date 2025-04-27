3eea383cd782612dba029a4a07d42301c7dee3d0
William Huan(STB) <chee_qin_huan_from.scientec@stb.gov.sg>
2025-04-27 22:07:11 +0800

    revert to 2.1.3-1

--- common/rootfs/docker-entrypoint.sh
+++ common/rootfs/docker-entrypoint.sh
@@ -47,7 +47,19 @@ else
 fi
 
 export ZIGBEE2MQTT_DATA="$(bashio::config 'data_path')"
-mkdir -p "$ZIGBEE2MQTT_DATA" || bashio::exit.nok "Could not create $ZIGBEE2MQTT_DATA"
+if ! bashio::fs.file_exists "$ZIGBEE2MQTT_DATA/configuration.yaml"; then
+    mkdir -p "$ZIGBEE2MQTT_DATA" || bashio::exit.nok "Could not create $ZIGBEE2MQTT_DATA"
+
+    cat <<EOF > "$ZIGBEE2MQTT_DATA/configuration.yaml"
+version: 4
+homeassistant:
+  enabled: true
+advanced:
+  network_key: GENERATE
+  pan_id: GENERATE
+  ext_pan_id: GENERATE
+EOF
+fi
 
 if bashio::config.has_value 'watchdog'; then
     export Z2M_WATCHDOG="$(bashio::config 'watchdog')"
@@ -55,15 +67,7 @@ if bashio::config.has_value 'watchdog'; then
 fi
 
 export NODE_PATH=/app/node_modules
-export ZIGBEE2MQTT_CONFIG_FRONTEND_ENABLED='true'
-export ZIGBEE2MQTT_CONFIG_FRONTEND_PORT='8099'
-export ZIGBEE2MQTT_CONFIG_HOMEASSISTANT_ENABLED='true'
-export Z2M_ONBOARD_URL='http://0.0.0.0:8099'
-
-if bashio::config.true 'force_onboarding'; then
-    export Z2M_ONBOARD_FORCE_RUN="1"
-    bashio::log.info "Forcing onboard to run"
-fi
+export ZIGBEE2MQTT_CONFIG_FRONTEND='{"enabled":true,"port": 8099}'
 
 if bashio::config.true 'disable_tuya_default_response'; then
     bashio::log.info "Disabling TuYa default responses"
--- zigbee2mqtt/CHANGELOG.md
+++ zigbee2mqtt/CHANGELOG.md
@@ -1,9 +1,3 @@
-## 2.2.1-1
-- Updated Zigbee2MQTT to version [`2.2.1`](https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.2.1)
-
-## 2.2.0-1
-- Updated Zigbee2MQTT to version [`2.2.0`](https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.2.0)
-
 ## 2.1.3-1
 - Updated Zigbee2MQTT to version [`2.1.3`](https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.1.3)
 
